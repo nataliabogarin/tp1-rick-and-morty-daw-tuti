c3ff4d471f928f6aa7e71c351d6c55f1f9fcfa10
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2026-09-25 21:18:06 -0300

    dato bd

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?PHP
 
 $ipBD = "127.0.0.1";
-$usuarioBD = "rick_and_morty"; 
-$claveBD = "root";            
+$usuarioBD = "root"; 
+$claveBD = "admin";            
 $nombreBD = "rick_and_morty"; 
 try {
     $conexion = new PDO("mysql:host=" . $ipBD . ";dbname=" . $nombreBD, $usuarioBD, $claveBD);
     
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "conexion exitosa";
 } catch (PDOException $e) {
     
    die("Error al conectar con la base de datos: " . $e->getMessage());
